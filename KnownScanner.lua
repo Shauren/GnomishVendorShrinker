8edc6603d0d848c52ee7e9f513355e509805756a
Shauren <shauren.trinity@gmail.com>
2024-01-04 20:02:14 +0100

    10.2.0 fix

--- KnownScanner.lua
+++ KnownScanner.lua
@@ -6,13 +6,24 @@ local function HasHeirloom(id)
 	return C_Heirloom.IsItemHeirloom(id) and C_Heirloom.PlayerHasHeirloom(id)
 end
 
+local function GetNestedItemTooltipLineCount(itemID)
+	local data = C_TooltipInfo.GetItemByID(itemID)
+	if data then
+		return #data.lines;
+	end
+	return 0
+end
 
-local function IsKnown(link)
-	ns.scantip:SetHyperlink(link)
-	for i=1,ns.scantip:NumLines() do
-		local line = ns.scantip.L[i]
-		if line == ITEM_SPELL_KNOWN then return true end
-		if line == ERR_COSMETIC_KNOWN then return true end
+function IsKnown(link)
+	local data = C_TooltipInfo.GetHyperlink(link)
+	local blockUntil = 1;
+	for i=1,#data.lines do
+		if i >= blockUntil then
+			local line = data.lines[i]
+			if line.type == Enum.TooltipDataLineType.NestedBlock and line.tooltipType == Enum.TooltipDataType.Item then blockUntil = i + GetNestedItemTooltipLineCount(line.tooltipID) end
+			if line.type == Enum.TooltipDataLineType.RestrictedSpellKnown then return true end
+			if line.leftText == ERR_COSMETIC_KNOWN then return true end
+		end
 	end
 end
 
@@ -33,9 +44,9 @@ ns.knowns = setmetatable({}, {
 -- "Requires Previous Rank"
 local PREV_RANK = TOOLTIP_SUPERCEDING_SPELL_NOT_KNOWN
 local function NeedsRank(link)
-	ns.scantip:SetHyperlink(link)
-	for i=1,ns.scantip:NumLines() do
-		if ns.scantip.L[i] == PREV_RANK then return true end
+	local data = C_TooltipInfo.GetHyperlink(link)
+	for _,line in ipairs(data.lines) do
+		if line.leftText == PREV_RANK then return true end
 	end
 end
 
